a4f2f0a4a0980d03ac96127b2a6f5ca4f4dd7b3e
Ashketchum03 <bharatpandey141@gamil.com>
2021-05-30 13:10:53 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# techknow.github.io
+# portfolio_bp
+Hey I'm Bharat pandey Software Developer From India
---
+++ main.js
@@ -0,0 +1,14 @@
+/** 
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    window.onload = screen;
+    window.onresize = screen;
+
+    function screen (){
+ 
+        myWidth = window.innerWidth;
+        document.getElementById('size').innerHTML = 'Width : '+myWidth+'px';
+    }
+
+});
+*/
